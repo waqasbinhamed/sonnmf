ab3bcc40f90810f855d8f7af5c4aa96d489bb5d1
Waqas <waqasbinhamed@gmail.com>
2023-05-09 17:21:05 -0400

    commited everything local

--- sonnmf/main.py
+++ sonnmf/main.py
@@ -1,25 +1,59 @@
-import numpy as np
-from sonnmf.utils import sonnmf_ini, sonnmf_post_it
-from sonnmf.update_H import base as update_H_base, precomputed_constants_with_nesterov as update_H_accelarated
-from sonnmf.update_W import update_matrix as update_W_mat
+from sonnmf.utils import ini_sonnmf, calculate_scores_and_report
+from sonnmf.update_H import base as base_H_func, precomputed_vars_and_nesterov_acc
+from sonnmf.update_W import base as base_W
+EARLY_STOP_TOL = 1e-6
 
 
-def base(M, W, H, lam=0.0, w_update_method='proximal_averaging', h_update_accelarated=False, itermin=100, itermax=10000, h_update_iters=5, w_update_iters=1, early_stop=True, verbose=False, scale_reg=False):
-    fscores, gscores, lambda_vals = sonnmf_ini(M, W, H, lam, itermax, scale_reg)
+def sonnmf(M, W, H, lam=0.0, gamma=0.0, itermin=100, itermax=10000, H_update_iters=1,
+           W_update_iters=100, accelerate_H_update=False, early_stop=True, verbose=False):
+    """
+    Performs non-negative matrix factorization with penalties on W and H.
+
+    Parameters:
+    - M: the matrix to be factorized
+    - W: the initial matrix for W
+    - H: the initial matrix for H
+    - lam: the penalty parameter for the group sparsity term
+    - gamma: the penalty parameter for the non-negativity constraint on W
+    - itermin: the minimum number of iterations to perform
+    - itermax: the maximum number of iterations to perform
+    - H_update_iters: the number of iterations to perform when updating H
+    - wj_update_iters: the number of iterations to perform when updating the j-th column of W
+    - early_stop: whether to use early stopping based on the change in objective function value
+    - verbose: whether to print progress updates
+
+    Returns:
+    - W: the learned matrix W
+    - H: the learned matrix H
+    - fscores: an array of the objective function value at each iteration
+    - gscores: an array of the group sparsity term value at each iteration
+    - hscores: an array of the non-negativity constraint violation value at each iteration
+    - total_scores: an array of the total objective function value at each iteration
+    """
+
+    m, n = M.shape
+    _, rank = W.shape
+
+    fscores, gscores, hscores, total_scores = ini_sonnmf(itermax)
+
+    it = 0
+    calculate_scores_and_report(H, M, W, fscores, gamma, gscores, hscores, it, lam, total_scores, verbose)
 
     for it in range(1, itermax + 1):
         # update H
-        if h_update_accelarated:
-            H = update_H_accelarated(M, W, H)
+        if accelerate_H_update:
+            H = precomputed_vars_and_nesterov_acc(H, H_update_iters, M, W, it)
         else:
-            H = update_H_base(M, W, H)
+            H = base_H_func(H, H_update_iters, M, W)
 
         # update W
-        W = update_W_mat(M, W, H, lambda_vals[it - 1], method=w_update_method)
+        base_W(H, M, W, W_update_iters, gamma, lam, m, rank)
 
-        fscores, gscores, lambda_vals, stop_now = sonnmf_post_it(M, W, H, it, fscores, gscores, lambda_vals,
-                                                                  early_stop, verbose, scale_reg, lam, itermin)
-        if stop_now:
-            break
+        calculate_scores_and_report(H, M, W, fscores, gamma, gscores, hscores, it, lam, total_scores, verbose)
 
-    return W, H, fscores[:it + 1], gscores[:it + 1], np.r_[np.NaN, lambda_vals[1: it + 1]]
+        if early_stop and it > itermin:
+            if abs(total_scores[it] - total_scores[it - 1]) / total_scores[it - 1] < EARLY_STOP_TOL:
+                print(f'Early stopping condition reached at iteration {it}.')
+                break
+
+    return W, H, fscores[:it + 1], gscores[:it + 1], hscores[:it + 1], total_scores[:it + 1]
---
+++ sonnmf/old/update_H.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sonnmf.old.utils import non_neg
+
+def proj_simplex(y):
+    return non_neg(y - np.max((np.cumsum(-1 * np.sort(-y, axis=0), axis=0) - 1) /
+                              np.arange(1, y.shape[0] + 1).reshape(y.shape[0], 1), axis=0))
+
+
+def base(M, W, H, iters=1):
+
+    for it in range(iters):
+        H = proj_simplex(H - ((W.T @ W) @ H - W.T @ M) / np.linalg.norm(W.T @ W, ord=2))
+    return H
+
+
+def precomputed_constants(M, W, H, iters=1):
+    WtW = W.T @ W
+    norm_WtW = np.linalg.norm(WtW, ord=2)
+    Q = np.identity(W.shape[1]) - (WtW / norm_WtW)
+    R = (W.T @ M) / norm_WtW
+
+    for it in range(iters):
+        H = proj_simplex((Q @ H) + R)
+    return H
+
+
+def precomputed_constants_with_nesterov(M, W, H, iters=1):
+    WtW = W.T @ W
+    norm_WtW = np.linalg.norm(WtW, ord=2)
+    Q = np.identity(W.shape[1]) - (WtW / norm_WtW)
+    R = (W.T @ M) / norm_WtW
+
+
+    V = H
+    for it in range(iters):
+        H_old = H
+        H = proj_simplex((Q @ V) + R)
+        V = H + ((it-1)/(it+2)) * (H - H_old)
+    return H
+
