432fbd41b2a4639d71d30e002c7b4f2d4fda1b2a
Waqas <waqasbinhamed@gmail.com>
2023-05-20 21:22:07 -0400

    further testing of the numba version

--- experimental/notebooks/numba_test.py
+++ experimental/notebooks/numba_test.py
@@ -3,10 +3,10 @@ import time
 import numpy as np
 from sonnmf.main import sonnmf
 from sonnmf.numba.main import sonnmf as sonnmf_numba
-data_filepath = '../datasets/jasper_small_6.npz'
-ini_filepath = '../saved_models/jasper_small_6/r{}_ini.npz'
-save_filepath = '../saved_models/jasper_small_6/r{}_l{}_g{}_it{}.npz'
-image_filepath = '../images/jasper_small_6/r{}_l{}_g{}_it{}.jpg'
+data_filepath = '../datasets/jasper_full.npz'
+ini_filepath = '../saved_models/jasper_full/r{}_ini.npz'
+save_filepath = '../saved_models/jasper_full/r{}_l{}_g{}_it{}.npz'
+image_filepath = '../images/jasper_full/r{}_l{}_g{}_it{}.jpg'
 M = np.load(data_filepath)['X']
 M = M.astype(np.float64)
 m, n = M.shape
@@ -23,16 +23,18 @@ else:
     with open(ini_filepath.format(r), 'wb') as fout:
         np.savez_compressed(fout, ini_W=ini_W, ini_H=ini_H)
 
-st = time.time()
-W, H, fscores, gscores, hscores, total_scores = sonnmf(M, ini_W.copy(), ini_H.copy(), lam=1, gamma=1, itermax=100, 
+start_time = time.time()
+W, H, fscores, gscores, hscores, total_scores = sonnmf(M, ini_W.copy(), ini_H.copy(), lam=1, gamma=1, itermax=10,
                                                        W_update_iters=10, early_stop=False, verbose=True)
-print(1, time.time() - st)
+print('Time taken by basic implementation:', time.time() - start_time)
 
 
+start_time = time.time()
 W, H, fscores, gscores, hscores, total_scores = sonnmf_numba(M, ini_W.copy(), ini_H.copy(), lam=1, gamma=1, itermax=1, 
                                                        W_update_iters=1, early_stop=False, verbose=True)
+print('Compilation time:', time.time() - start_time)
 
-st = time.time()
-W, H, fscores, gscores, hscores, total_scores = sonnmf_numba(M, ini_W.copy(), ini_H.copy(), lam=1, gamma=1, itermax=100, 
+start_time = time.time()
+W, H, fscores, gscores, hscores, total_scores = sonnmf_numba(M, ini_W.copy(), ini_H.copy(), lam=1, gamma=1, itermax=10,
                                                        W_update_iters=10, early_stop=False, verbose=True)
-print(2, time.time() - st)
+print('Time taken by numba implementation:', time.time() - start_time)
