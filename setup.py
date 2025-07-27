ebbc2bd470a5ccc5452fd3944f3c107720a7531a
Waqas Bin Hamed <waqasbinhamed@gmail.com>
2025-07-27 18:38:55 -0400

    set up as package

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
 from setuptools import setup, find_packages
 
-with open("README.md", mode="r", encoding="utf-8") as readme_file:
+with open("README.md", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-setup(name='sonnmf',
-      version='0.21',
-      description='Python implementation of NMF-SON.',
-      url='https://github.com/waqasbinhamed/sonnmf',
-      author='Waqas Bin Hamed',
-      author_email='waqasbinhamed@gmail.com',
-      packages=find_packages(),
-      python_requires='>=3.6.0',
-      install_requires=['numpy', 'scipy']
-      )
+setup(
+    name='sonnmf',
+    version='1.0.0',
+    description='Python implementation of Sum-of-norms regularized Nonnegative Matrix Factorization (SONNMF)',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url='https://github.com/waqasbinhamed/sonnmf',
+    author='Waqas Bin Hamed',
+    author_email='waqasbinhamed@gmail.com',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy>=1.18',
+        'scipy>=1.4'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Natural Language :: English"
+    ],
+)
